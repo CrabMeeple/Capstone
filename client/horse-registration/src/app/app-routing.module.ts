04e7e77669b1ef9b9ad3d1bb5085563dc0ae5281
Tyler Yake <tyryake@gmail.com>
2021-09-14 09:47:38 -0500

    Add fallback route

--- client/horse-registration/src/app/app-routing.module.ts
+++ client/horse-registration/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 import { HorseFormEditComponent } from './horse-form-edit/horse-form-edit.component';
 import { HorseFormComponent } from './horse-form/horse-form.component';
 import { RaceFormEditComponent } from './race-form-edit/race-form-edit.component';
@@ -7,7 +7,9 @@ import { RaceFormComponent } from './race-form/race-form.component';
 import { RacesComponent } from './races/races.component';
 import { TracksComponent } from './tracks/tracks.component';
 
-
+const fallbackRoute: Route = {
+  path: '**', component: TracksComponent
+}
 const routes: Routes = [{
   path: '',
   children: [
@@ -16,7 +18,8 @@ const routes: Routes = [{
     {path: 'races/addEntrant', component:HorseFormComponent},
     {path: 'races/editEntrant', component:HorseFormEditComponent},
     {path: 'races/addRace', component:RaceFormComponent},
-    {path: 'races/editRace', component:RaceFormEditComponent}
+    {path: 'races/editRace', component:RaceFormEditComponent},
+    fallbackRoute
   ]
 }];
 
